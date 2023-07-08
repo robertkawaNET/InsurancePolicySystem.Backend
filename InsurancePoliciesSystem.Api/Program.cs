7dce2e5309a2c2960e17f038354d1b9f3be0f578
robert.kawa <robertkawa1992@gmail.com>
2023-07-08 09:31:12 +0200

    Added swagger

--- InsurancePoliciesSystem.Api/Program.cs
+++ InsurancePoliciesSystem.Api/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
---
+++ InsurancePoliciesSystem.Api/SellPolicies/WorkInsuranceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsurancePoliciesSystem.Api.SellPolicies;
+
+[ApiController]
+[Route("api/sell-policies/work-insurance")]
+public class WorkInsuranceController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Test() => Ok("test");
+}
